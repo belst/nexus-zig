7088110549ef20b7f6b66093fcc33d1782b11121
belst <git@bel.st>
2025-07-10 23:17:00 +0200

    imgui

--- src/root.zig
+++ src/root.zig
@@ -2,13 +2,38 @@ const std = @import("std");
 const imgui = @import("imgui");
 const nexus = @import("nexus");
 
+fn render() callconv(.C) void {
+    if (imgui.ImGui_Begin("zig test", null, 0)) {
+        const S = struct {
+            var open: bool = true;
+        };
+        if (S.open) {
+            S.open = !imgui.ImGui_Button("hide");
+            imgui.ImGui_Text("zig test");
+        } else {
+            S.open = imgui.ImGui_Button("show");
+        }
+    }
+    imgui.ImGui_End();
+}
+
+var api: *nexus.AddonAPI = undefined;
+
 fn load(a: ?*nexus.AddonAPI) callconv(.C) void {
     if (a) |ap| {
+        ap.*.Log.?(nexus.ELogLevel_TRACE, "zig_test", "setting up imgui context and allocators");
+        imgui.ImGui_SetCurrentContext(@ptrCast(ap.*.ImguiContext));
+        imgui.ImGui_SetAllocatorFunctions(@ptrCast(ap.*.ImguiMalloc), @ptrCast(ap.*.ImguiFree), null);
         ap.*.Log.?(nexus.ELogLevel_TRACE, "zig_test", "zig test loaded");
+        ap.*.Renderer.Register.?(nexus.ERenderType_Render, &render);
+        ap.*.Log.?(nexus.ELogLevel_TRACE, "zig_test", "zig renderer registered");
+        api = ap;
     }
 }
 
-fn unload() callconv(.C) void {}
+fn unload() callconv(.C) void {
+    api.*.Renderer.Deregister.?(&render);
+}
 
 pub export fn GetAddonDef() callconv(.C) *const nexus.AddonDefinition {
     return &.{
